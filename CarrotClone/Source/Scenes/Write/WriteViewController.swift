0f5b021476d34c91abcdc4ceb88a0b889bbc296b
sbear123 <sbear123@naver.com>
2021-07-25 13:37:20 +0900

    [INIT 글 작성 파일 생성

---
+++ CarrotClone/Source/Scenes/Write/WriteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WriteViewController.swift
+//  CarrotClone
+//
+//  Created by 박지현 on 2021/07/24.
+//
+
+import UIKit
+
+class WriteViewController: UIViewController {
+    var coordinator: WriteCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ CarrotClone/Source/Scenes/Write/WriteViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  WriteViewModel.swift
+//  CarrotClone
+//
+//  Created by 박지현 on 2021/07/24.
+//
+
+import Foundation
+
+class WriteViewModel {
+    
+}
