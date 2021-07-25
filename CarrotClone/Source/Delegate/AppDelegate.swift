c73c289af029093e1b56477e6ddf117e5b79b892
sbear123 <sbear123@naver.com>
2021-07-25 13:30:13 +0900

    [UPDATE] 처음 화면 전환 구현

--- CarrotClone/Source/Delegate/AppDelegate.swift
+++ CarrotClone/Source/Delegate/AppDelegate.swift
@@ -11,12 +11,12 @@ import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
         return true
     }
+    
 
     // MARK: UISceneSession Lifecycle
 
